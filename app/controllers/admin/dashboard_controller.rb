6989a822568b44bca3f273070da1c86dcc55a847
Ekaterina <yatakshilina@gmail.com>
2023-07-07 13:02:19 -0400

    add Feature: Admin Categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
+    puts "HERE", Category.inspect
     @category_count = Category.count
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  # namespace :admin do
+  #   get 'categories/index'
+  #   get 'categories/new'
+  #   get 'categories/create'
+  # end
+
+  # namespace :admin do
+  #   resources :categories, only: [:index, :new, :create]
+  # end
+
+  namespace :admin do
+    root to: 'dashboard#show'
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
+  end
+  
   get 'about/index'
   root to: 'products#index'
 
@@ -16,10 +32,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
-  end
+  # namespace :admin do
+  #   root to: 'dashboard#show'
+  #   resources :products, except: [:edit, :update, :show]
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
